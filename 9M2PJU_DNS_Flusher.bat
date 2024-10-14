0c5d07b80a0aa7d6556ab14c3d4325b6d28f782b
9M2PJU <9m2pju@gmail.com>
2024-10-15 01:37:23 +0800

    Update 9M2PJU_DNS_Flusher.bat

--- 9M2PJU_DNS_Flusher.bat
+++ 9M2PJU_DNS_Flusher.bat
@@ -2,20 +2,19 @@
 
 echo.
 TITLE 9M2PJU DNS Flusher
-echo.
-echo.
+
 echo dP""Yb 8b    d8 oP"Yb. 88""Yb  88888 88   88 
 echo Ybood8 88b  d88 "' dP' 88__dP     88 88   88 
 echo   .8P' 88YbdP88   dP'  88"""  o.  88 Y8   8P 
 echo  .dP'  88 YY 88 .d8888 88     "bodP' `YbodP'  
 echo.    
 echo.
-echo Installing 9M2PJU DNS Flusher to Startup folder...
+echo  Installing 9M2PJU DNS Flusher to Startup folder...
 timeout /t 3 /nobreak  > nul 
 
 cd /D "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" 
 echo @echo off ^& title Flushing DNS, please wait a bit... ^& ipconfig /flushdns > 9M2PJU_DNS_Flusher.bat
-echo Adding 9M2PJU DNS Flusher to the Startup folder
+echo  Adding flusher to the Startup folder
 timeout /t 3 /nobreak  > nul
 echo.
 echo  ^>^> Thanks for installing 9M2PJU DNS Flusher!
